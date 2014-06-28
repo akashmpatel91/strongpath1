3ae53aa00b57bb157663877c7b90992b038475da
akashp <akash.p@infibeam.net>
2014-06-28 15:27:13 +0530

    Get all likers

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,56 +19,60 @@ class WelcomeController < ApplicationController
     puts like_hash
     i = 0
     j = 1
-    response = HTTParty.get("https://graph.facebook.com/755985781119509/feed/?access_token=CAACEdEose0cBABlP2ZB2G8qDHDfTVwEZApCeoA2LHRGkgWP7SJV607ZBCUvXfEZCGqPEvBut1NSXTxFeZA4yYVaJJbkEo7AC5NWhZAUs5kY2zMlzii2iGNZAeeSFlyLsKka2GWWlfktRqWH9D0kD8HxVHNqJZA8WSfGrkZBo0BlGsHZADuSg23LS7ZASCPiqnqPzrIZD")
+    response = HTTParty.get("https://graph.facebook.com/755985781119509/feed/?access_token=CAACEdEose0cBAKRCwGYWGgs33XjZCbJO4yDMYZCYugnlOxqWqJueBA5E0AAhI28gRBDxRZCU1PEldprn67p0psauZCOcDIchFG9yTiIZCBS1M4XKc8pBsc4ZAXT151yZAqSxrfXgqseeZCthvpy9oETHNenZCGL4LZBIJ5c1WpM4DGSntgiSWw3CtL4cOh2eLlC8kZD")
     puts "putting response"
     json = JSON.parse(response.body)
-    get_common_map(response,common_hash)
-    puts common_hash
+    get_common_map(response, common_hash)
 
     if json['error'] == nil || json['error'] == ''
       json['data'].each do |d|
         d['likes']['data'].each do |x|
           get_likers(x, 0, like_hash)
         end
-        #puts like_hash
-        #puts like_hash
         d['likes']['paging'].each do |x|
           j = j+1
           next if j.even?
           if x[0] == "next"
-            response1 = HTTParty.get(x[1])
-            json = JSON.parse(response1.body)
-            json['data'].each do |y|
-              y.map do |a, b|
-                if b.match(/^\d+$/)
-                  if like_hash[b] == nil
-                    like_hash[b] =1
-                  else
-                    like_hash[b] = like_hash[b] + 1
-                  end
-                end
-              end
-            end
-            #get_likers(json['data'], 0, like_hash)
-            #getLikers(json, 0, like_hash)
+            puts "Outside function" + x[1]
+            likers_page(x[1], like_hash)
           end
 
         end
       end
       puts "Final Mapppppppppppppppp#################################"
       puts like_hash
-      like_hash.clear
 
     else
       puts "dingoooo.."
     end
 
 
-
-
   end
 
-  def get_common_map(response,common_hash)
+  def likers_page(url,like_hash)
+    response1 = HTTParty.get(url)
+    json = JSON.parse(response1.body)
+    json['data'].each do |y|
+      y.map do |a, b|
+        if b.match(/^\d+$/)
+          if like_hash[b] == nil
+            like_hash[b] =1
+          else
+            like_hash[b] = like_hash[b] + 1
+          end
+        end
+      end
+    end
+    if json['paging']['next']
+      puts "In the recursion"
+      puts like_hash
+      likers_page(json['paging']['next'],like_hash)
+    else
+      puts "Out from recursion"
+    end
+  end
+
+  def get_common_map(response, common_hash)
     counter = Hash.new
     counter = response.body.strip.downcase.split(/[^\w']+/).group_by(&:to_s).map { |w| {w[0] => w[1].count} }
     counter.each do |c|
