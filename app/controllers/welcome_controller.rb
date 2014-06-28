f7c55048668959a4788dd8f254699a58609f619b
akashp <akash.p@infibeam.net>
2014-06-28 18:27:35 +0530

    Get commentors

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
   require 'httparty'
   require 'json'
 
+  @@retryCount = 0
 
   def index
     #@graph = Koala::Facebook::API.new(current_user.oauth_token)
@@ -19,57 +20,89 @@ class WelcomeController < ApplicationController
     puts like_hash
     i = 0
     j = 1
-    response = HTTParty.get("https://graph.facebook.com/755985781119509/feed/?access_token=CAACEdEose0cBAKRCwGYWGgs33XjZCbJO4yDMYZCYugnlOxqWqJueBA5E0AAhI28gRBDxRZCU1PEldprn67p0psauZCOcDIchFG9yTiIZCBS1M4XKc8pBsc4ZAXT151yZAqSxrfXgqseeZCthvpy9oETHNenZCGL4LZBIJ5c1WpM4DGSntgiSWw3CtL4cOh2eLlC8kZD")
+    response = HTTParty.get("https://graph.facebook.com/755985781119509/feed/?limit=3000&access_token=CAACEdEose0cBAMppwM6sSdb7Qju8qjfFxHXiVFL0qCIuL2iFOURqqC1Gl3KOMZCAqaCN3NeGbYOyLahIArID65Mg84ZBB80s3HoGgWBWCjnan1oyD0OKKF05QgjP4UbduUyif6b3H8MFxQKoC0dJA2EZBHswN75RcVZCQGXC7XZCyDtHMXDAHFAyFrZBMWxQ4ZD")
     puts "putting response"
     json = JSON.parse(response.body)
     get_common_map(response, common_hash)
 
     if json['error'] == nil || json['error'] == ''
       json['data'].each do |d|
-        d['likes']['data'].each do |x|
-          get_likers(x, 0, like_hash)
-        end
-        d['likes']['paging'].each do |x|
-          j = j+1
-          next if j.even?
-          if x[0] == "next"
-            puts "Outside function" + x[1]
-            likers_page(x[1], like_hash)
-          end
-
-        end
+        like_injector(d, j, like_hash)
+        comment_injector(comment_hash, d)
       end
       puts "Final Mapppppppppppppppp#################################"
       puts like_hash
+      puts comment_hash
 
     else
       puts "dingoooo.."
     end
 
-
   end
 
-  def likers_page(url,like_hash)
-    response1 = HTTParty.get(url)
-    json = JSON.parse(response1.body)
-    json['data'].each do |y|
-      y.map do |a, b|
-        if b.match(/^\d+$/)
-          if like_hash[b] == nil
-            like_hash[b] =1
-          else
-            like_hash[b] = like_hash[b] + 1
-          end
+  def comment_injector(comment_hash, d)
+    if d['comments']
+      d['comments']['data'].each do |x|
+        if comment_hash[x['from']['id']] == nil
+          comment_hash[x['from']['id']] =1
+        else
+          comment_hash[x['from']['id']] = comment_hash[x['from']['id']] + 1
         end
       end
     end
-    if json['paging']['next']
-      puts "In the recursion"
-      puts like_hash
-      likers_page(json['paging']['next'],like_hash)
-    else
-      puts "Out from recursion"
+  end
+
+  def like_injector(d, j, like_hash)
+    d['likes']['data'].each do |x|
+      get_likers(x, 0, like_hash)
     end
+    d['likes']['paging'].each do |x|
+      j = j+1
+      next if j.even?
+      if x[0] == "next"
+        puts "Outside function" + x[1]
+        likers_page(x[1], like_hash)
+      end
+
+    end
+  end
+
+  def likers_page(url, like_hash)
+    begin
+      response1 = HTTParty.get(url)
+      if response1
+        json = JSON.parse(response1.body)
+        json['data'].each do |y|
+          y.map do |a, b|
+            if b.match(/^\d+$/)
+              if like_hash[b] == nil
+                like_hash[b] =1
+              else
+                like_hash[b] = like_hash[b] + 1
+              end
+            end
+          end
+        end
+        if json['paging']['next']
+          puts "In the recursion"
+          likers_page(json['paging']['next'], like_hash)
+        else
+          puts "Out from recursion"
+        end
+      else
+        puts "Failed inside....."
+      end
+    rescue => ex
+      @@retryCount += 1
+      if @@retryCount <= 5
+        puts "Exception...count" + @@retryCount.to_s
+        likers_page(url, like_hash)
+      else
+        puts "Died... :("
+        puts ex.message
+      end
+    end
+
   end
 
   def get_common_map(response, common_hash)
@@ -83,6 +116,7 @@ class WelcomeController < ApplicationController
 
       end
     end
+    #puts common_hash
   end
 
   def get_likers(x, i, like_hash)
@@ -94,6 +128,20 @@ class WelcomeController < ApplicationController
         like_hash[y[1]] =1
       else
         like_hash[y[1]] = like_hash[y[1]] + 1
+      end
+    end
+
+  end
+
+  def get_commentors(x, i, comment_hash)
+    x.each do |y|
+      i = i+1
+      next if i.even?
+      #puts y[1]
+      if comment_hash[y[1]] == nil
+        comment_hash[y[1]] =1
+      else
+        comment_hash[y[1]] = comment_hash[y[1]] + 1
       end
     end
 
