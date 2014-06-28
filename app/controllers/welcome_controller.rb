f8580968fad0a65c27c0537b378d838033625f73
akashp <akash.p@infibeam.net>
2014-06-28 13:32:23 +0530

    add map

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
   require 'httparty'
   require 'json'
 
+
   def index
     #@graph = Koala::Facebook::API.new(current_user.oauth_token)
     #@friends = @graph.get_connections("me", "feed")
@@ -11,27 +12,24 @@ class WelcomeController < ApplicationController
   end
 
   def parse
+    like_hash = {}
+    comment_hash = {}
+    common_hash = {}
+    puts common_hash
+    puts like_hash
     i = 0
     j = 1
-    response = HTTParty.get("https://graph.facebook.com/755985781119509/feed/?access_token=CAACEdEose0cBAJlIwmDgQzLe3LMj9QlO7NhPMrzfg3NH6nEecVZAbnBsFewKYUJYcIWklLVBMy9jkdztVhx6Iwos0TnC2hD0oxlZBg1PTZAJDOxShxhYECIa5taFkKKFx3fDiXw0HRfSWnkbkt6VKgpZBgMV1rOmAUgbcTGgSv82OJAbyMB4fnUXZCW257E0ZD")
+    response = HTTParty.get("https://graph.facebook.com/755985781119509/feed/?access_token=CAACEdEose0cBABlP2ZB2G8qDHDfTVwEZApCeoA2LHRGkgWP7SJV607ZBCUvXfEZCGqPEvBut1NSXTxFeZA4yYVaJJbkEo7AC5NWhZAUs5kY2zMlzii2iGNZAeeSFlyLsKka2GWWlfktRqWH9D0kD8HxVHNqJZA8WSfGrkZBo0BlGsHZADuSg23LS7ZASCPiqnqPzrIZD")
     puts "putting response"
     json = JSON.parse(response.body)
-    #response = open("")
-    counter = Hash.new
-    counter = response.body.strip.downcase.split(/[^\w']+/).group_by(&:to_s).map { |w| {w[0] => w[1].count} }
-    counter.each do |c|
-      c.map do |x, y|
-        #puts "Key is " + x
-        #puts  y
-      end
-    end
-    like_hash = {}
-    if json != nil
+    get_common_map(response,common_hash)
+    puts common_hash
+
+    if json['error'] == nil || json['error'] == ''
       json['data'].each do |d|
         d['likes']['data'].each do |x|
           get_likers(x, 0, like_hash)
         end
-        puts "Likers on the main page--------------------------------------------------"
         #puts like_hash
         #puts like_hash
         d['likes']['paging'].each do |x|
@@ -40,15 +38,15 @@ class WelcomeController < ApplicationController
           if x[0] == "next"
             response1 = HTTParty.get(x[1])
             json = JSON.parse(response1.body)
-            puts "Filling next page likers-----------------------------------------"
             json['data'].each do |y|
-              i = i+1
-              next if i.even?
-              puts y[1]
-              if like_hash[y[1]] == nil
-                like_hash[y[1]] =1
-              else
-                like_hash[y[1]] = like_hash[y[1]] + 1
+              y.map do |a, b|
+                if b.match(/^\d+$/)
+                  if like_hash[b] == nil
+                    like_hash[b] =1
+                  else
+                    like_hash[b] = like_hash[b] + 1
+                  end
+                end
               end
             end
             #get_likers(json['data'], 0, like_hash)
@@ -56,12 +54,31 @@ class WelcomeController < ApplicationController
           end
 
         end
-        puts "Likers in another page..................................................."
       end
       puts "Final Mapppppppppppppppp#################################"
-      #puts like_hash
+      puts like_hash
+      like_hash.clear
+
+    else
+      puts "dingoooo..."
     end
 
+
+
+
+  end
+
+  def get_common_map(response,common_hash)
+    counter = Hash.new
+    counter = response.body.strip.downcase.split(/[^\w']+/).group_by(&:to_s).map { |w| {w[0] => w[1].count} }
+    counter.each do |c|
+      c.map do |x, y|
+        if x.match(/^\d+$/)
+          common_hash[x] = y
+        end
+
+      end
+    end
   end
 
   def get_likers(x, i, like_hash)
